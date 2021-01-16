b3f513a5727ddbb80f7581550d8f47bf4f62532c
80andAbove <shogomiyagi94@gmail.com>
2021-01-16 23:25:16 +0900

    Fixed the WorkShift Error and Implemented Changes

--- lifesaver/models.py
+++ lifesaver/models.py
@@ -16,7 +16,7 @@ class WorkShift(models.Model):
     end_datetime = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.name 
 
 #Personel Related aka Employees and Patients
 
@@ -24,7 +24,8 @@ class Doctor(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
-    department = models.ForeignKey(Department, default="", null=True, blank=True, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, null=True, blank=True, on_delete=models.CASCADE)
+    work_shift = models.OneToOneField(WorkShift, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -33,12 +34,12 @@ class Nurse(models.Model):
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
-    department = models.ForeignKey(Department, default="", null=True, blank=True, on_delete=models.CASCADE)
-    reports_to = models.OneToOneField(Doctor, default="", blank=True, null=True, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, null=True, blank=True, on_delete=models.CASCADE)
+    reports_to = models.ForeignKey(Doctor, blank=True, null=True, on_delete=models.CASCADE)
     work_shift = models.OneToOneField(WorkShift, default="", blank=True, null=True, on_delete=models.CASCADE)
-
-    # def __str__(self):
-    #     return self.name if self.name else ''
+    
+    def __str__(self):
+         return self.name
 
 class Patient(models.Model):
     STATUS = (
